a19c9d9402c8c49ddecb0811b00d9c35ea0f954d
Angiras Menon <angirasmenon@gmail.com>
2022-06-07 14:58:39 -0400

    angirasm: Added torch version of MinMaxScaler.

--- conf_solv/dataloaders/scalers.py
+++ conf_solv/dataloaders/scalers.py
@@ -17,7 +17,7 @@ class TorchStandardScaler(nn.Module):
         return x
 
     def inverse_transform(self,x):
-        x * x * (self.std + 1e-7)
+        x = x * (self.std + 1e-7)
         x = x + self.mean
         return x
 
@@ -36,7 +36,7 @@ class TorchMinMaxScaler(nn.Module):
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
 
-    def __call__(self, tensor):
+    def fit(self, tensor):
         """Fit features
 
         Parameters
@@ -50,25 +50,24 @@ class TorchMinMaxScaler(nn.Module):
             A tensor with scaled features using requested preprocessor.
         """
 
-        tensor = torch.stack(tensor)
+        #tensor = torch.stack(tensor)
 
         # Feature range
-        a, b = self.feature_range
-
-        dist = tensor.max(dim=0, keepdim=True)[0] - tensor.min(dim=0, keepdim=True)[0]
-        dist[dist == 0.0] = 1.0
-        scale = 1.0 / dist
-        tensor.mul_(scale).sub_(tensor.min(dim=0, keepdim=True)[0])
+        
+        self.max = tensor.max(dim=0, keepdim=True)[0]
+        self.min = tensor.min(dim=0, keepdim=True)[0]
+        self.dist = self.max - self.min
+        self.dist[self.dist == 0.0] = 1.0
+        self.scale = 1.0 / self.dist
+    def transform(self,tensor):  
+        a, b = self.feature_range 
+        tensor.mul_(self.scale).sub_(tensor.min(dim=0, keepdim=True)[0])
         tensor.mul_(b - a).add_(a)
-
+        return tensor
+    def inverse_transform(self,tensor):
+        a, b = self.feature_range 
+        tensor.sub_(a).div_(b - a)
+        tensor.add_(tensor.min(dim=0, keepdim=True)[0]).div_(self.scale)   
+        tensor.add_(self.min[0])     
         return tensor
 
-#Let's do some testing: 
-# data = torch.tensor([[0, 0], [0, 0], [1, 1], [1, 1]]).float()
-# foo = TorchStandardScaler()
-# foo.fit(data)
-# print(foo.transform(data))
-# data_np = data.numpy()
-# scaler = StandardScaler()
-# arr_norm = scaler.fit_transform(data_np)
-# print(arr_norm)
