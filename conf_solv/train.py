4c19f284180a29d370d5db7bd04a104916bee249
Kevin Spiekermann <kspieker@mit.edu>
2022-06-16 11:32:26 -0400

    Reorganize code for saving test predictions

--- conf_solv/train.py
+++ conf_solv/train.py
@@ -7,11 +7,11 @@ from pytorch_lightning.profiler import PyTorchProfiler
 from conf_solv.dataloaders.loader import SolventData3DModule
 from conf_solv.trainer import LitConfSolvModule
 import sys
-import torch
-import pandas as pd
 
 import os
 from argparse import ArgumentParser
+
+from .utils.inference_utils import save_predictions
 
 if 'linux' in sys.platform:
     import resource  # for ancdata error
@@ -68,70 +68,11 @@ def train_conf_solv(config):
     # get predictions on the test set
     predictions_all_batches = trainer.predict(ckpt_path='best')
     scaler = trainer.datamodule.scaler
-
-    unscaled_preds_all = torch.tensor([])
-    y_true_all = torch.tensor([])
-    test_dict_info = {'mol_id': [], 
-                      'conf_id': [],
-                      'solvent': [], 
-                      'dG_pred (kJ/mol)': [],
-                      'dG_true (kJ/mol)': [],
-                     }
-    # iterate over the batches
-    for data_batch, preds_batch in zip(solvation_data.predict_dataloader(), predictions_all_batches):
-        preds = preds_batch['preds'].squeeze()
-        y_true = data_batch.y
-        unscaled_preds = scaler.inverse_transform(preds)
-        normalized_coeff = data_batch.solute_mask.sum()
-        assert normalized_coeff == len(unscaled_preds)
-        
-        # make sure that the lowest energy conf is labeled as energy 0
-        lowest_idx = y_true.argmin()
-        y_true_0_offset = y_true - y_true[lowest_idx]
-        
-        # make sure that the lowest energy conf is labeled as energy 0
-        lowest_idx = unscaled_preds.argmin()
-        unscaled_preds_0_offset = unscaled_preds - unscaled_preds[lowest_idx]
-
-        unscaled_preds_all = torch.cat((unscaled_preds_all, unscaled_preds_0_offset))
-        y_true_all = torch.cat((y_true_all, y_true_0_offset))
-                
-        for i, conf_id in enumerate(data_batch.conf_ids[0]):
-            test_dict_info['mol_id'].append(data_batch.mol_id[0])
-            test_dict_info['solvent'].append(data_batch.solvent_name[0])    
-            test_dict_info['conf_id'].append(conf_id)
-    
-    test_dict_info['dG_pred (kJ/mol)'] = unscaled_preds_all.detach().numpy()
-    test_dict_info['dG_true (kJ/mol)'] = y_true_all.detach().numpy()
-    df_test_info = pd.DataFrame(test_dict_info)
-    df_test_info.to_csv(os.path.join(f"{config['log_dir']}", 'test_predictions.csv'), index=False)
-
-    # group the test errors by solvent
-    stats_dict = {'solvent': df_test_info.solvent.unique().tolist(),
-                  'MAE (kJ/mol)': [],
-                  'RMSE (kJ/mol)': [],
-                 }
-    for solvent in stats_dict['solvent']:
-        df_tmp = df_test_info[df_test_info.solvent == solvent]
-        preds = df_tmp['dG_pred (kJ/mol)'].values
-        true = df_tmp['dG_true (kJ/mol)'].values
-        
-        rmse = np.sqrt(sum((preds - true)**2) / len(true))
-        mae = np.abs(preds - true).sum() / len(true)
-        stats_dict['MAE (kJ/mol)'].append(mae)
-        stats_dict['RMSE (kJ/mol)'].append(rmse)
-
-    preds = df_test_info['dG_pred (kJ/mol)'].values
-    true = df_test_info['dG_true (kJ/mol)'].values
-    rmse = np.sqrt(sum((preds - true)**2) / len(true))
-    mae = np.abs(preds - true).sum() / len(true)
-
-    stats_dict['solvent'].append('Overall')
-    stats_dict['MAE (kJ/mol)'].append(mae)
-    stats_dict['RMSE (kJ/mol)'].append(rmse)
-    df_stats = pd.DataFrame(stats_dict)
-    df_stats.to_csv(os.path.join(f"{config['log_dir']}", 'test_stats.csv'), index=False)
-
+    save_predictions(predictions=predictions_all_batches,
+                     dataloader=solvation_data.predict_dataloader(),
+                     scaler=scaler,
+                     config=config,
+                     )
 
 if __name__ == "__main__":
     parser = ArgumentParser()
